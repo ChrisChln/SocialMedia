028f4c39eedaff1fdc8042f100d00375b4dba59e
Linnan Chen <tus11309@temple.edu>
2025-01-28 09:47:07 -0500

    Add files via upload
    Front-end deployment and back-end deployment

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f5f5f5;
+}
+
+#app {
+  min-height: 100vh;
+}
+</style>
